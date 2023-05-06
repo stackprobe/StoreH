55e2a14aab6935a52c1f484974bf18b3af3780f1
stackprobe <stackprobes@gmail.com>
2023-05-06 17:25:03 +0900

    Voyager 1 is 23914280119.572 kilometers away from the Sun.

---
+++ Dev/Dough/00_ConsoleForTest/Claes20200001/Claes20200001/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using Charlotte.Commons;
+using Charlotte.Tests;
+
+namespace Charlotte
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ProcMain.CUIMain(new Program().Main2);
+		}
+
+		// 以下様式統一のため用途別に好きな方を使ってね -- ★要削除
+
+#if true // 主にデバッガで実行するテスト用プログラム -- ★不要なら要削除
+		private void Main2(ArgsReader ar)
+		{
+			if (ProcMain.DEBUG)
+			{
+				Main3();
+			}
+			else
+			{
+				Main4();
+			}
+			SCommon.OpenOutputDirIfCreated();
+		}
+
+		private void Main3()
+		{
+			Main4();
+			SCommon.Pause();
+		}
+
+		private void Main4()
+		{
+			try
+			{
+				Main5();
+			}
+			catch (Exception ex)
+			{
+				ProcMain.WriteLog(ex);
+			}
+		}
+
+		private void Main5()
+		{
+			// -- choose one --
+
+			new Test0001().Test01();
+			//new Test0002().Test01();
+			//new Test0003().Test01();
+
+			// --
+		}
+#else // 主に実行ファイルにして使う/コマンド引数有り -- ★不要なら要削除
+		private void Main2(ArgsReader ar)
+		{
+			if (ProcMain.DEBUG)
+			{
+				Main3();
+			}
+			else
+			{
+				Main4(ar);
+			}
+			SCommon.OpenOutputDirIfCreated();
+		}
+
+		private void Main3()
+		{
+			// テスト系 -- リリース版では使用しない。
+#if DEBUG
+			// -- choose one --
+
+			Main4(new ArgsReader(new string[] { }));
+			//new Test0001().Test01();
+			//new Test0002().Test01();
+			//new Test0003().Test01();
+
+			// --
+#endif
+			SCommon.Pause();
+		}
+
+		private void Main4(ArgsReader ar)
+		{
+			try
+			{
+				Main5(ar);
+			}
+			catch (Exception ex)
+			{
+				ProcMain.WriteLog(ex);
+
+				//MessageBox.Show("" + ex, Path.GetFileNameWithoutExtension(ProcMain.SelfFile) + " / エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+				//Console.WriteLine("Press ENTER key. (エラーによりプログラムを終了します)");
+				//Console.ReadLine();
+			}
+		}
+
+		private void Main5(ArgsReader ar)
+		{
+			// TODO
+		}
+#endif
+	}
+}
