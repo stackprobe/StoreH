bc3d47e53abe4b1cecb90324c9b7a9c341be2c5d
stackprobe <stackprobes@gmail.com>
2023-05-09 02:40:21 +0900

    Voyager 1 is 23917761592.093 kilometers away from the Sun.

--- Dev/Program/Test20230501/Claes20200001/Claes20200001/Program.cs
+++ Dev/Program/Test20230501/Claes20200001/Claes20200001/Program.cs
@@ -68,7 +68,8 @@ namespace Charlotte
 			//new Test0002().Test08();
 			//new Test0003().Test01();
 			//new Test0004().Test01();
-			new Test0004().Test02();
+			//new Test0004().Test02();
+			new Test0005().Test01();
 
 			// --
 		}
---
+++ Dev/Program/Test20230501/Claes20200001/Claes20200001/Tests/Test0005.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte.Tests
+{
+	public class Test0005
+	{
+		public void Test01()
+		{
+			Console.WriteLine(double.PositiveInfinity); // ∞
+
+			double value = double.Parse("∞");
+			Console.WriteLine(value); // ∞
+
+			Console.WriteLine(double.NegativeInfinity); // -∞
+
+			value = double.Parse("-∞");
+			Console.WriteLine(value); // -∞
+
+			Console.WriteLine(double.NaN); // NaN
+
+			value = double.Parse("NaN");
+			Console.WriteLine(value); // NaN
+		}
+	}
+}
