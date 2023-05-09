215a58ec44ffc02f19ad3c0d96c6e22169f77743
stackprobe <stackprobes@gmail.com>
2023-05-09 16:36:36 +0900

    Voyager 1 is 23918609043.326 kilometers away from the Sun.

--- Dev/Dough/UnitTest/Claes20200001/Claes20200001/Tests/Test0012.cs
+++ Dev/Dough/UnitTest/Claes20200001/Claes20200001/Tests/Test0012.cs
@@ -16,6 +16,34 @@ namespace Charlotte.Tests
 			Console.WriteLine(SCommon.SimpleDateTime.Now().ToDateTime());
 			Console.WriteLine(new SCommon.SimpleDateTime(SCommon.SimpleDateTime.Now().ToDateTime()));
 
+			// ----
+
+			SCommon.SimpleDateTime dt = SCommon.SimpleDateTime.Now();
+
+			Console.WriteLine(dt); // Now
+			Console.WriteLine(dt++); // Now
+			Console.WriteLine(dt++); // Now + 1
+			Console.WriteLine(dt++); // Now + 2
+			Console.WriteLine(dt--); // Now + 3
+			Console.WriteLine(dt--); // Now + 2
+			Console.WriteLine(dt--); // Now + 1
+
+			Console.WriteLine(dt); // Now
+			Console.WriteLine(++dt); // Now + 1
+			Console.WriteLine(++dt); // Now + 2
+			Console.WriteLine(++dt); // Now + 3
+			Console.WriteLine(--dt); // Now + 2
+			Console.WriteLine(--dt); // Now + 1
+			Console.WriteLine(--dt); // Now
+
+			Console.WriteLine(dt); // Now
+			Console.WriteLine(dt += 10); // Now + 10
+			Console.WriteLine(dt -= 20); // Now - 10
+			Console.WriteLine(dt += 10); // Now
+			Console.WriteLine(dt); // Now
+
+			// ----
+
 			Console.WriteLine("done! (TEST-0012-01)");
 		}
 	}
